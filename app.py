8e78724c8ddbad3cea187c3c9c8d4aa88e1429dd
johninator3250i <john@team938.com>
2024-09-11 23:05:04 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return 'This is the about page!'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000, host='127.0.0.1')
