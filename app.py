ddd949e0732d9feeb158a2aa29b92d308e63c30a
johninator3250i <john@team938.com>
2024-09-21 00:08:24 -0400

    Added layout, blocks, and extends. Created route links with url_for.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
@@ -9,9 +9,13 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/about')
-def about():
-    return 'This is the about page!'
+@app.route('/pet')
+def pet():
+    return render_template('pet.html')
+
+@app.route('/add-pet')
+def add_pet():
+    return render_template('addpet.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000, host='127.0.0.1')
